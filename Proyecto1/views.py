383466d0ec38428e8a1a1fdf19f3fa4ec4e356af
above9ll <juanmfranco1928@gmail.com>
2026-04-10 10:43:26 -0500

    Plantilla sobre plantillas

--- Proyecto1/views.py
+++ Proyecto1/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 import datetime
 from django.template import Template, Context
 from django.template.loader import get_template  #CARGADOR DE PLANTILLAS
+from django.shortcuts import render
 
 class Persona(object):
     def __init__(self, nombre, apellido):
@@ -22,13 +23,11 @@ def saludo(request):
     # doc_externo=open("C:/Users/JuanManuelFrancoRuiz/OneDrive - 900053370_AFFI SAS/Escritorio/ProyectosDjango/Proyecto1/plantillas/miplantilla.html")
     # plt=Template(doc_externo.read())
     # doc_externo.close()
-
-    doc_externo=loader.get_template('miplantilla.html') #CARGAR PLANTILLAS
-
+    # doc_externo=get_template('miplantilla.html') #CARGAR PLANTILLAS
     # ctx=Context({"nombre_persona":p1.nombre, "apellido_persona":p1.apellido, "momento_actual":ahora, "temas":temasDelCurso})
-    documento=doc_externo.render({"nombre_persona":p1.nombre, "apellido_persona":p1.apellido, "momento_actual":ahora, "temas":temasDelCurso})
+    # documento=doc_externo.render({"nombre_persona":p1.nombre, "apellido_persona":p1.apellido, "momento_actual":ahora, "temas":temasDelCurso})
 
-    return HttpResponse(documento)
+    return render(request, "miplantilla.html", {"nombre_persona":p1.nombre, "apellido_persona":p1.apellido, "momento_actual":ahora, "temas":temasDelCurso})
 
 def despedida(request):
     return HttpResponse("Hasta luego alumnos")
